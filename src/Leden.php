bd269984df5a508b1c63c5fd3fa5938aa3e03f52
Björn Boekhoorn <bjoern.boekhoorn@rocdeleijgraaf.nl>
2016-02-17 13:00:04 +0100

    Hi Tim

---
+++ src/Leden.php
@@ -0,0 +1,10 @@
+<div class="col-10 table">
+	<table class="col-12">
+		<tr>
+			<td class="titel" colspan="4"><h1>Groep 1A</h1></td>
+		</tr>
+		<tr>
+			<th class="naam">Naam</th><th class="achternaam">Achternaam</th><th class="plaats">Woonplaats</th><th class="email">Email</th>
+		</tr>
+	</table>
+</div>
