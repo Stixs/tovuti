6151b097278bc164aa943036225a3dcbae1773e9
Björn Boekhoorn <bjoern.boekhoorn@rocdeleijgraaf.nl>
2016-02-25 10:07:21 +0100

    Aanpassing

--- src/AdminLedenWijzigen.php
+++ src/AdminLedenWijzigen.php
@@ -156,47 +156,48 @@
 			{
 	
 ?>
-
-				<div class="col-0_5"></div>
-				<div class="col-11 tableALW">
-					<?php
-						$sth = $pdo->prepare('SELECT * FROM groep');
-						$sth->execute();
-						
-						while($row = $sth->fetch())
-						{
-							echo '<table class="col-12">';
-							echo '<tr>';
-							echo '<th colspan="10">'.$row['Groepnaam'].'</th>';
-							echo '</tr>';
-							$groep = $row['GroepID'];
-							echo '<th>Achternaam</th><th>Voornaam</th><th>Adres</th><th>Woonplaats</th><th>Leeftijd</th><th>Groep</th><th>Email</th><th>Telefoon</th><th>Wijzigen</th>';
+				<div class="row ledentabel">
+					<div class="col-0_5"></div>
+					<div class="col-11 tableALW">
+						<?php
+							$sth = $pdo->prepare('SELECT * FROM groep');
+							$sth->execute();
 							
-							$parameters = array(':GroepID'=>$groep);
-							$sth1 = $pdo->prepare('SELECT * FROM persoonsgegevens p, groepsleiders gl, groep g WHERE gl.PersoonsID = p.PersoonsID AND gl.GroepID = g.GroepID AND gl.GroepID = :GroepID');
-							$sth1->execute($parameters);
-							$row1 = $sth1->fetch();
-							if(isset($row1['PersoonsID']))
+							while($row = $sth->fetch())
 							{
+								echo '<table class="col-12">';
 								echo '<tr>';
-								echo '<td>'.$row1['Achternaam'].'</td><td>'.$row1['Voornaam'].'</td><td>'.$row1['Adres'].'</td><td>'.$row1['Woonplaats'].'</td><td>'.$row1['Leeftijd'].'</td><td>'.$row1['Groepnaam'].'</td><td>'.$row1['Email'].'</td><td>'.$row1['Telefoon'].'</td><td><a href="./index.php?Page=10&Action=Edit&PID='.$row1['PersoonsID'].'">Wijzigen</a></td>';
+								echo '<th colspan="10">'.$row['Groepnaam'].'</th>';
 								echo '</tr>';
+								$groep = $row['GroepID'];
+								echo '<th>Achternaam</th><th>Voornaam</th><th>Adres</th><th>Woonplaats</th><th>Leeftijd</th><th>Groep</th><th>Email</th><th>Telefoon</th><th>Wijzigen</th>';
+								
+								$parameters = array(':GroepID'=>$groep);
+								$sth1 = $pdo->prepare('SELECT * FROM persoonsgegevens p, groepsleiders gl, groep g WHERE gl.PersoonsID = p.PersoonsID AND gl.GroepID = g.GroepID AND gl.GroepID = :GroepID');
+								$sth1->execute($parameters);
+								$row1 = $sth1->fetch();
+								if(isset($row1['PersoonsID']))
+								{
+									echo '<tr>';
+									echo '<td>'.$row1['Achternaam'].'</td><td>'.$row1['Voornaam'].'</td><td>'.$row1['Adres'].'</td><td>'.$row1['Woonplaats'].'</td><td>'.$row1['Leeftijd'].'</td><td>'.$row1['Groepnaam'].'</td><td>'.$row1['Email'].'</td><td>'.$row1['Telefoon'].'</td><td><a href="./index.php?Page=10&Action=Edit&PID='.$row1['PersoonsID'].'" class="button">Wijzigen</a></td>';
+									echo '</tr>';
+								}
+								
+								$parameters = array(':GroepID'=>$groep);
+								$sth2 = $pdo->prepare('SELECT * FROM persoonsgegevens p, leden l, groep g WHERE l.PersoonsID = p.PersoonsID AND l.GroepID = :GroepID AND l.GroepID = g.GroepID');
+								$sth2->execute($parameters);
+								
+								while($row2 = $sth2->fetch())
+								{
+									echo '<tr>';
+									echo '<td>'.$row2['Achternaam'].'</td><td>'.$row2['Voornaam'].'</td><td>'.$row2['Adres'].'</td><td>'.$row2['Woonplaats'].'</td><td>'.$row2['Leeftijd'].'</td><td>'.$row2['Groepnaam'].'</td><td>'.$row2['Email'].'</td><td>'.$row2['Telefoon'].'</td><td><a href="./index.php?Page=10&Action=Edit&PID='.$row2['PersoonsID'].'" class="button">Wijzigen</a></td>';
+									echo '</tr>';
+								}
+								
+								echo '</table>';
 							}
-							
-							$parameters = array(':GroepID'=>$groep);
-							$sth2 = $pdo->prepare('SELECT * FROM persoonsgegevens p, leden l, groep g WHERE l.PersoonsID = p.PersoonsID AND l.GroepID = :GroepID AND l.GroepID = g.GroepID');
-							$sth2->execute($parameters);
-							
-							while($row2 = $sth2->fetch())
-							{
-								echo '<tr>';
-								echo '<td>'.$row2['Achternaam'].'</td><td>'.$row2['Voornaam'].'</td><td>'.$row2['Adres'].'</td><td>'.$row2['Woonplaats'].'</td><td>'.$row2['Leeftijd'].'</td><td>'.$row2['Groepnaam'].'</td><td>'.$row2['Email'].'</td><td>'.$row2['Telefoon'].'</td><td><a href="./index.php?Page=10&Action=Edit&PID='.$row2['PersoonsID'].'">Wijzigen</a></td>';
-								echo '</tr>';
-							}
-							
-							echo '</table>';
-						}
-					?>
+						?>
+					</div>
 				</div>
 <?php
 			}
--- src/MijnGroep.php
+++ src/MijnGroep.php
@@ -1,21 +1,42 @@
 <div class="row ledentabel">
 	<div class="col-1 "></div>
 	<div class="col-10">
-		<table class="col-12">
-			<tr>
-				<td class="titel" colspan="4"><h1>Groep 1A</h1></td>
-			</tr>
+		<table class="col-12 MijnGroep">
+			<?php
+				$parameters = array(':PID'=>$_SESSION['user_id']);
+				$sth = $pdo->prepare('SELECT * FROM groep g, leden l WHERE l.PersoonsID = :PID AND l.GroepID = g.GroepID');
+				$sth->execute($parameters);
+				$row = $sth->fetch();
+				
+				$groep = $row['GroepID'];
+				
+				echo '<tr>';
+				echo '<td class="titel" colspan="5"><h1>Groep '.$row['Groepnaam'].'</h1></td>';
+				echo '</tr>';
+			?>
 			<tr>
 				<th class="naam">Naam</th><th class="achternaam">Achternaam</th><th class="plaats">Woonplaats</th><th class="email">Email</th><th class="telefoon">Telefoon</th>
 			</tr>
 			<?php
-				$sth = $pdo->prepare("SELECT * FROM persoonsgegevens");
-				$sth->execute();
+				$parameters = array (':GID'=>$groep);
+				$sth = $pdo->prepare("SELECT * FROM persoonsgegevens p, groepsleiders gl WHERE p.PersoonsID = gl.PersoonsID AND gl.GroepID = :GID");
+				$sth->execute($parameters);
 				
 				while($row = $sth->fetch())
 				{
 					echo '<tr>';
 					echo '<td>'.$row['Voornaam'].'</td><td>'.$row['Achternaam'].'</td><td>'.$row['Woonplaats'].'</td><td>'.$row['Email'].'</td><td>'.$row['Telefoon'].'</td>';
+					echo '</tr>';
+				}
+				
+				$parameters1 = array (':GID'=>$groep);
+				$sth1 = $pdo->prepare("SELECT * FROM persoonsgegevens p, leden l WHERE p.PersoonsID = l.PersoonsID AND l.GroepID = :GID");
+				$sth1->execute($parameters1);
+				
+				while($row1 = $sth1->fetch())
+				{
+					echo '<tr>';
+					echo '<td>'.$row1['Voornaam'].'</td><td>'.$row1['Achternaam'].'</td><td>'.$row1['Woonplaats'].'</td><td>'.$row1['Email'].'</td><td>'.$row1['Telefoon'].'</td>';
 					echo '</tr>';
 				}
 			?>
