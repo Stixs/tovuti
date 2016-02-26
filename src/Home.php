4bc0d9a4ee875c2521d2dbe92b8c6d8d15b8d49f
Björn Boekhoorn <bjoern.boekhoorn@rocdeleijgraaf.nl>
2016-02-26 11:49:18 +0100

    laatste loodjes lololol

--- src/Home.php
+++ src/Home.php
@@ -1,2 +1,19 @@
-<h2>Home</h2>
-<img src="./docs/logo.png" width="50%" id="logoHome" />
+
+<div class="row home">
+	<p>Welkom op de leden administratie site van FC IORN.</p>
+	<p>Log in om gebruik te kunnen maken van deze site.</p>
+</div>
+<div class="row">
+	<div class="col-0_5"></div>
+	<div class="col-3">
+		<img src="http://www.argeweb-video.nl/wp-content/uploads/2015/10/voetbal.jpg" width="100%" height="187.767px" class="logoHome" />
+	</div>
+	<div class="col-1"></div>
+	<div class="col-3">
+		<img src="./docs/logo.png" width="100%" class="logoHome" />
+	</div>
+	<div class="col-1"></div>
+	<div class="col-3">
+		<img src="http://www.l1.nl/sites/default/files/imagenodes/voetbal_in_net_0.jpg" width="100%" height="187.767px" class="logoHome" />
+	</div>
+</div>
